55d07bbb51ac3143d3a54d85eb8b4f848e95bac9
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2023-09-07 18:40:01 -0400

    decresed camel spawnrates

--- AlathraMobs/src/main/java/me/ShermansWorld/alathramobs/listeners/CreatureSpawnListener.java
+++ AlathraMobs/src/main/java/me/ShermansWorld/alathramobs/listeners/CreatureSpawnListener.java
@@ -22,8 +22,8 @@ public class CreatureSpawnListener implements Listener {
 		Location loc = e.getEntity().getLocation();
 		if (BiomeUtil.getCamelBiomes().contains(BiomeUtil.getBiomeName(loc))) {
 			double chance = Math.random();
-			// 1 in 25 chance
-			if (chance > 0.96) {
+			// 1 in 100 chance
+			if (chance > 0.99) {
 				e.getEntity().getWorld().spawnEntity(loc, EntityType.CAMEL);
 				e.setCancelled(true);
 			}
