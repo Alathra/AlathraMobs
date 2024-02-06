ca0f3244a3c9e0b486a037a8e87805eef8d065c2
Xavier Beatrice <Xavbeat03@gmail.com>
2024-02-06 01:46:29 -0500

    Changing the ShawnSummoners Array from using Players to using UUID's, also fixing the problem where it still took a item when you were in creative.

--- AlathraMobs/src/main/java/me/ShermansWorld/alathramobs/ItemsListener.java
+++ AlathraMobs/src/main/java/me/ShermansWorld/alathramobs/ItemsListener.java
@@ -13,12 +13,13 @@ import org.bukkit.inventory.ItemStack;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class ItemsListener implements Listener {
 	/**
 	 * A list of players who've summoned shawn
 	 */
-	ArrayList<Player> shawnSummoners = new ArrayList<>();
+	ArrayList<UUID> shawnSummoners = new ArrayList<>();
 
 
 	/**
@@ -46,13 +47,13 @@ public class ItemsListener implements Listener {
 				double clickedBlockZ = e.getClickedBlock().getZ();
 
 
-				HashMap<Location, Material> LocationMaterialPairs = new HashMap<>();
-				LocationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX+1, clickedBlockY, clickedBlockZ), Material.GOLD_BLOCK);
-				LocationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX-1, clickedBlockY, clickedBlockZ), Material.GOLD_BLOCK);
-				LocationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX, clickedBlockY, clickedBlockZ+1), Material.GOLD_BLOCK);
-				LocationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX, clickedBlockY, clickedBlockZ-1), Material.GOLD_BLOCK);
-				LocationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX, clickedBlockY+1, clickedBlockZ), Material.AIR);
-				LocationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX, clickedBlockY+2, clickedBlockZ), Material.AIR);
+				HashMap<Location, Material> locationMaterialPairs = new HashMap<>();
+				locationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX+1, clickedBlockY, clickedBlockZ), Material.GOLD_BLOCK);
+				locationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX-1, clickedBlockY, clickedBlockZ), Material.GOLD_BLOCK);
+				locationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX, clickedBlockY, clickedBlockZ+1), Material.GOLD_BLOCK);
+				locationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX, clickedBlockY, clickedBlockZ-1), Material.GOLD_BLOCK);
+				locationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX, clickedBlockY+1, clickedBlockZ), Material.AIR);
+				locationMaterialPairs.put(new Location(e.getClickedBlock().getWorld(), clickedBlockX, clickedBlockY+2, clickedBlockZ), Material.AIR);
 
 
 
@@ -64,24 +65,26 @@ public class ItemsListener implements Listener {
 					// Looks for custom model data with a value of 301
 					if (item.getItemMeta().hasCustomModelData()) {
 						if (item.getItemMeta().getCustomModelData() == 301) {
-							if(shawnSummoners.contains(e.getPlayer()) && e.getPlayer().getGameMode() != GameMode.CREATIVE){
+							if(shawnSummoners.contains(e.getPlayer().getUniqueId()) && e.getPlayer().getGameMode() != GameMode.CREATIVE){
 								e.getPlayer().sendMessage("You have already summoned them today...");
 								e.setCancelled(true);
 								return;
 							}
 							// stops placing the wool without the structure
-							for(Map.Entry<Location, Material> locationMaterialEntry: LocationMaterialPairs.entrySet()){
+							for(Map.Entry<Location, Material> locationMaterialEntry: locationMaterialPairs.entrySet()){
 								if(locationMaterialEntry.getKey().getBlock().getType() != locationMaterialEntry.getValue()){
 									e.setCancelled(true);
 									return;
 								}
 							}
-							// Removing the item
-							if (item.getAmount() > 1) {
-								item.setAmount(item.getAmount() - 1);
-								e.getPlayer().getInventory().setItemInMainHand(item);
-							} else {
-								e.getPlayer().getInventory().remove(item);
+							if(e.getPlayer().getGameMode() != GameMode.CREATIVE) {
+								// Removing the item
+								if (item.getAmount() > 1) {
+									item.setAmount(item.getAmount() - 1);
+									e.getPlayer().getInventory().setItemInMainHand(item);
+								} else {
+									e.getPlayer().getInventory().remove(item);
+								}
 							}
 							// Actually doing the summoning
 
@@ -91,12 +94,12 @@ public class ItemsListener implements Listener {
 							e.getClickedBlock().getWorld().createExplosion(e.getClickedBlock().getLocation(), 5);
 
 							Location shawnSummonLocation = e.getClickedBlock().getLocation();
-							shawnSummonLocation.setY(shawnSummonLocation.getBlockY()+1); // summons Shawn 1 block above the center of the structure
+							shawnSummonLocation.setY(shawnSummonLocation.getBlockY()+1.0); // summons Shawn 1 block above the center of the structure
 							MobsUtil.spawnMob("Nether_Sheep_Red", shawnSummonLocation);
 							e.getPlayer().getWorld().playSound(e.getPlayer().getLocation(),
 								Sound.ENTITY_SHEEP_HURT, 5F, 5F);
 
-							shawnSummoners.add(e.getPlayer()); //stops player from summoning multiple times a restart
+							shawnSummoners.add(e.getPlayer().getUniqueId()); //stops player from summoning multiple times a restart
 						}
 
 					}
